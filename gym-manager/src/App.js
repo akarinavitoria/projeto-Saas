70c34e43535d30fcde026e4352a0eb6609c14754
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2024-11-21 17:03:46 +0000

    .

--- gym-manager/src/App.js
+++ gym-manager/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import ClientsList from './pages/Clients/ClientsList';
+import Header from './components/Header/Header.js';
+import ClientsList from 'src/pages/Clients/ClientsList';
 import './styles/global.css'; // Certifique-se de criar e configurar este arquivo global de estilos.
 
 function App() {
