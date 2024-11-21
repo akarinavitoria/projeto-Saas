ef73f98ac73a30a7ce6d7b5ac72516704f21eb7b
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2024-11-21 17:32:45 +0000

    .

--- gym-manager/src/App.js
+++ gym-manager/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import Header from './components/Header/Header.js';
-import ClientsList from './pages/clients/ClientsList';
-import './styles/global.css'; // Certifique-se de criar e configurar este arquivo global de estilos.
+import Header from './components/Header/Header';
+import ClientsList from './pages/Clients/ClientsList';
+import './styles/global.css';
 
 function App() {
-  return <h1>Welcome to Gym Manager!</h1>;
+  return (
+    <div>
+      <Header />
+      <ClientsList />
+    </div>
+  );
 }
 
 export default App;
+
 
 
