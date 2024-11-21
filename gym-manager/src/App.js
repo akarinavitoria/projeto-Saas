a97396281dde4eb205839269e0f8011d66f26e97
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2024-11-21 17:13:53 +0000

    .

--- gym-manager/src/App.js
+++ gym-manager/src/App.js
@@ -4,14 +4,7 @@ import ClientsList from 'src/pages/Clients/ClientsList';
 import './styles/global.css'; // Certifique-se de criar e configurar este arquivo global de estilos.
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <ClientsList />
-      </main>
-    </>
-  );
+  return <h1>Welcome to Gym Manager!</h1>;
 }
 
 export default App;
