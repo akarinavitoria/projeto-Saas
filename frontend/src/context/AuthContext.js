f4faf44dc9ca08cbc82467a9f74e866f34393b0e
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2024-12-19 13:39:17 +0000

    .

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from 'react';
+import jwtDecode from 'jwt-decode';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (token) => {
+    const decodedUser = jwtDecode(token);
+    setUser(decodedUser);
+    localStorage.setItem('token', token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
