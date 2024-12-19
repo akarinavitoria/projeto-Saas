fde0a811bccdfc0e6157541c277586044f57824c
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2024-12-19 12:54:10 +0000

    .

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Gyms from './pages/Gyms';
-import Registrations from './pages/Registrations';
-import Payments from './pages/Payments';
-import Search from './pages/Search';
-import NotFound from './pages/NotFound';
+import HomePage from './components/HomePage';
+import RegistrationPage from './components/RegistrationPage';
+import PaymentsPage from './components/PaymentsPage';
+import SearchPage from './components/SearchPage';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gyms" element={<Gyms />} />
-        <Route path="/registrations" element={<Registrations />} />
-        <Route path="/payments" element={<Payments />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/payments" element={<PaymentsPage />} />
+        <Route path="/search" element={<SearchPage />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
+
