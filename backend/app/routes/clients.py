e14b23e2bfe32d64860b4acf3c59d5c0e06527a7
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-01-12 00:14:05 +0000

    .

--- backend/app/routes/clients.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint("clients", __name__)
-
-@bp.route("/clients")
-def list_clients():
-    return "Lista de clientes"
--- backend/app/routes/professionals.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint("professionals", __name__)
-
-@bp.route("/professionals")
-def list_professionals():
-    return "Lista de Profissionais"
